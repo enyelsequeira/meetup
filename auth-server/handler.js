e9f6ae972e7547142da705deae4c0b29b384b22f
Enyel <enyelsequeira@hotmail.com>
2020-02-28 12:00:40 +0000

    4.3

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -11,13 +11,39 @@ module.exports.getAccessToken = async (event) => {
     + '&redirect_uri=https://enyelsequeira.github.io/meetup/'
     + '&code=' + event.pathParameters.code;
 
-    const info = await axios.post(MEETUP_OAUTH_URL);
+  const info = await axios.post(MEETUP_OAUTH_URL);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        access_token: info.data.access_token,
-        refresh_token: info.data.refresh_token,
-      }),
-    };
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
   };
+};
+
+module.exports.refreshAccessToken = async (event) => {
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
+  + '?client_id=rhn3t2og7hsen40fqej3nr42e6'
+  + '&client_secret=huv3l6nfgpr20r50ss0ccn1e12'
+  + '&grant_type=authorization_code'
+  + '&redirect_uri=https://enyelsequeira.github.io/meetup/'
+  + '&code=' + event.pathParameters.code;
+
+  const info = await axios.post(MEETUP_OAUTH_URL)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
+  };
+};
