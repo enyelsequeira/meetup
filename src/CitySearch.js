e8d2294a521030203632974185a60b2dad09de9e
unknown <enyelsequeira@hotmail.com>
2020-03-20 21:12:42 +0000

    testing for api response

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -9,22 +9,39 @@ class CitySearch extends Component {
     infoText: ''
   };
 
-  handleInputChanged = event => {
+
+  handleInputChanged = async (event) => {
     const value = event.target.value;
     this.setState({ query: value });
-    getSuggestions(value).then(suggestions => {
-      suggestions && this.setState({ suggestions })
-      if (value && this.state.suggestions.length === 0) {
-        this.setState({
-          infoText: ' We can not find the city you are looking for. Please try another city',
-        });
-      } else {
-        this.setState({
-          infoText: '',
-        });
-      }
-    })
+    const apiSuggestions = await getSuggestions(value);
+    this.setState({ suggestions: [...apiSuggestions] });
+    if (value && this.state.suggestions.length === 0) {
+      this.setState({
+        infoText: ' We can not find the city you are looking for. Please try another city',
+      });
+    } else {
+      this.setState({
+        infoText: '',
+      });
+    }
   };
+
+  // handleInputChanged = event => {
+  //   const value = event.target.value;
+  //   this.setState({ query: value });
+  //   getSuggestions(value).then(suggestions => {
+  //     suggestions && this.setState({ suggestions })
+  //     if (value && this.state.suggestions.length === 0) {
+  //       this.setState({
+  //         infoText: ' We can not find the city you are looking for. Please try another city',
+  //       });
+  //     } else {
+  //       this.setState({
+  //         infoText: '',
+  //       });
+  //     }
+  //   })
+  // };
 //   handleInputChanged = event => {
 //     const value = event.target.value;
 //     this.setState({ query: value });
