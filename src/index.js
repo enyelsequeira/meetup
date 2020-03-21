24ea07e3dd059974a51aef16cb8ce919128f8ff6
unknown <enyelsequeira@hotmail.com>
2020-03-21 14:04:17 +0000

    4.8 changin icon and index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 atatus.config('58db6860e5a9407b94af6934c1c00257').install();
 
